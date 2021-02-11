5742bd8b224632d880eb7ac9b55ce34cc584fdfb
Yuya Sumie <monamonacan0926@gmail.com>
2021-02-11 17:03:53 +0900

    protoc によるコード生成

--- pkg/grpc/v1/himo/proto/room_grpc.pb.go
+++ pkg/grpc/v1/himo/proto/room_grpc.pb.go
@@ -95,3 +95,86 @@ var _Room_serviceDesc = grpc.ServiceDesc{
 	Streams:  []grpc.StreamDesc{},
 	Metadata: "grpc/v1/himo/proto/room.proto",
 }
+
+// UserManagerClient is the client API for UserManager service.
+//
+// For semantics around ctx use and closing/ending streaming RPCs, please refer to https://pkg.go.dev/google.golang.org/grpc/?tab=doc#ClientConn.NewStream.
+type UserManagerClient interface {
+	SignUp(ctx context.Context, in *SignUpRequest, opts ...grpc.CallOption) (*SignUpResponse, error)
+}
+
+type userManagerClient struct {
+	cc grpc.ClientConnInterface
+}
+
+func NewUserManagerClient(cc grpc.ClientConnInterface) UserManagerClient {
+	return &userManagerClient{cc}
+}
+
+func (c *userManagerClient) SignUp(ctx context.Context, in *SignUpRequest, opts ...grpc.CallOption) (*SignUpResponse, error) {
+	out := new(SignUpResponse)
+	err := c.cc.Invoke(ctx, "/himo.v1.UserManager/SignUp", in, out, opts...)
+	if err != nil {
+		return nil, err
+	}
+	return out, nil
+}
+
+// UserManagerServer is the server API for UserManager service.
+// All implementations must embed UnimplementedUserManagerServer
+// for forward compatibility
+type UserManagerServer interface {
+	SignUp(context.Context, *SignUpRequest) (*SignUpResponse, error)
+	mustEmbedUnimplementedUserManagerServer()
+}
+
+// UnimplementedUserManagerServer must be embedded to have forward compatible implementations.
+type UnimplementedUserManagerServer struct {
+}
+
+func (UnimplementedUserManagerServer) SignUp(context.Context, *SignUpRequest) (*SignUpResponse, error) {
+	return nil, status.Errorf(codes.Unimplemented, "method SignUp not implemented")
+}
+func (UnimplementedUserManagerServer) mustEmbedUnimplementedUserManagerServer() {}
+
+// UnsafeUserManagerServer may be embedded to opt out of forward compatibility for this service.
+// Use of this interface is not recommended, as added methods to UserManagerServer will
+// result in compilation errors.
+type UnsafeUserManagerServer interface {
+	mustEmbedUnimplementedUserManagerServer()
+}
+
+func RegisterUserManagerServer(s grpc.ServiceRegistrar, srv UserManagerServer) {
+	s.RegisterService(&_UserManager_serviceDesc, srv)
+}
+
+func _UserManager_SignUp_Handler(srv interface{}, ctx context.Context, dec func(interface{}) error, interceptor grpc.UnaryServerInterceptor) (interface{}, error) {
+	in := new(SignUpRequest)
+	if err := dec(in); err != nil {
+		return nil, err
+	}
+	if interceptor == nil {
+		return srv.(UserManagerServer).SignUp(ctx, in)
+	}
+	info := &grpc.UnaryServerInfo{
+		Server:     srv,
+		FullMethod: "/himo.v1.UserManager/SignUp",
+	}
+	handler := func(ctx context.Context, req interface{}) (interface{}, error) {
+		return srv.(UserManagerServer).SignUp(ctx, req.(*SignUpRequest))
+	}
+	return interceptor(ctx, in, info, handler)
+}
+
+var _UserManager_serviceDesc = grpc.ServiceDesc{
+	ServiceName: "himo.v1.UserManager",
+	HandlerType: (*UserManagerServer)(nil),
+	Methods: []grpc.MethodDesc{
+		{
+			MethodName: "SignUp",
+			Handler:    _UserManager_SignUp_Handler,
+		},
+	},
+	Streams:  []grpc.StreamDesc{},
+	Metadata: "grpc/v1/himo/proto/room.proto",
+}
